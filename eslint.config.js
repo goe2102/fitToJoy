d324f771b37a3e56cd555f888a986cbd3cc17c84
Sebastian Göpfert <sebastian.goepfert@student.tugraz.at>
2026-04-07 12:00:11 +0200

    folder structure set up

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+module.exports = {
+  extends: 'expo',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never']
+  }
+}
